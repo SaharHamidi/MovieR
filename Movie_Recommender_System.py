2458be2869f8f59e43b6369817e7bb7e7033c780
nargesr564 <nargesrezaiiiii@gmail.com>
2024-05-03 14:07:42 +0330

    fixed function inputs & completed questionnaire part
    functions were not given appropriate inputs. Questionaire now takes answers and saves them in a list

--- Movie_Recommender_System.py
+++ Movie_Recommender_System.py
@@ -1,5 +1,4 @@
 
-
 #movies={"movie name":[year,IMDB ratings,director,cast,genre,time,rotten tomatoes]}
 movies={"Inception":[2010,9,"Christopher_Nolan",["Leonardo_Dicaprio","Cillian_Murphy","Tom_Hardy"],["action","sci-fi"],148,87],
         "The_dark_knight":[2008,9,"Christopher_Nolan",["Christian_Bale","Heath_Ledger","Gary_Oldman"],["action","crime"],152,84],
@@ -13,87 +12,108 @@ def welcome_page():
     print("\n")
     print("\t\t********** Welcome to movie club! **********")
     print("\t\t------------------------------------------")
-    input("\t\t\t  Press enter to continue...")
+    input("\t\t\t  Press enter to continue...\n")
 
-def create_profile(p={}):
+def create_profile(profile={}):
     
-    profile['Name'] = input(" What is your name? : ")
+    profile['Name'] = input(" What is your name?:")
     while (type(profile['Name'])!=str or profile['Name'].isalpha()==False):
-        profile['Name'] = input(" Please enter a valid name : ")
+        profile['Name'] = input(" Please enter a valid name:")
 
-    profile['LastName'] = input(" What is your Last name? : ")
+    profile['LastName'] = input(" What is your Last name?:")
     while (type(profile['LastName'])!=str or profile['LastName'].isalpha()==False):
-        profile['LastName'] = input(" Please enter a valid last name : ")
+        profile['LastName'] = input(" Please enter a valid last name:")
 
-    profile['Age'] = input(" How old are you? : ")
-    while (profile['Age'].isnumeric()==False or int(profile['Age'])>125):
-        profile['Age'] = input(" Please enter a valid number : ")
+    profile['Age'] = input(" How old are you?:")
+    while (profile['Age'].isnumeric()==False or int(profile['Age'])>125 or int(profile['Age'])<1):
+        profile['Age'] = input(" Please enter a valid number:")
     
-    profile['Email'] = input(" What is your email? : ")
+    profile['Email'] = input(" What is your email?:")
     #while (profile['Email'].endswith('@gmail.com',-10,-1)==False or profile['Email'].endswith('@yahoo.com',-10,-1)==False):
         #profile['Email'] = input(" Please enter a valid email ending with @gmail.com or @yahoo.com : ")
     #install re module 
-    profile['PhoneNumber'] = input (" What is your phone number? : ")
-    while (profile['PhoneNumber'].isnumeric()==False or len(profile['PhoneNumber'])>15):
-        profile['PhoneNumber'] = input(" Please enter a valid phone number : ")
+    profile['PhoneNumber'] = input (" What is your phone number?:")
+    while (profile['PhoneNumber'].isnumeric()==False or len(profile['PhoneNumber'])>15 or len(profile['PhoneNumber'])<11):
+        profile['PhoneNumber'] = input(" Please enter a valid phone number:")
     
-def show_profile():
+def show_profile(profile={}):
     print("\n Your profile:")
     for key, value in profile.items():
         print(f"{key}: {value}")
 
-def confirmation():
+def confirmation(profile={},fav=[]):
     confirm = input("Is the information correct?(yes/no)") 
     if confirm.lower() == 'no':
         profile={}
         create_profile(profile)
-        show_profile()
-        confirmation()
+        show_profile(profile)
+        confirmation(profile,fav)
     elif confirm.lower() == 'yes':
-        questionnaire()
+        questionnaire(fav)
     else:
-        confirmation()
+        confirmation(profile,fav)
         
         
-def verify_information():
+def verify_information(profile={},fav=[]):
     confirm = input("press v to verify your information or press s to skip:")
     if confirm.lower() == 'v':
-        show_profile()
-        confirmation()
+        show_profile(profile)
+        confirmation(profile,fav)
     elif confirm.lower() == 's':
-        questionnaire()
+        questionnaire(fav)
     else:
-        verify_information()
+        verify_information(profile,fav)
 
-def questionnaire():
+def questionnaire(favourites=[]):
     print("\nLet's find out what your preferences are.")
-    print("Please answer the next 10 questions by choosing an option or typing out your answer.\n")
+    print("Please answer the next 10 questions by choosing one of the options below.")
+    print("(for example:a)\n")
     #question 1
-    print("1) Do you prefer popular movies or indie films?\na)very popular shows\nb)indie movies\nc)doesn't matter to me\n")
+    print("1)Do you prefer popular movies or indie films?\na)very popular shows\nb)indie movies\nc)doesn't matter to me\nd)both\n")
+    favourites.append(input("answer:"))
     #question 2
-    print("2) What is the last series you watched that you really liked?\n")
+    print("\n2)Which rating do you think is most valid?\na)IMDB\nb)Rotten Tomatos\nc)neither\nd)both\n")
+    favourites.append(input("answer:"))
     #question 3
-    print("3) Choose two of the genres below that you enjoy the most:\na)comedy\nb)action\nc)drama\nd)horror\n")
+    print("\n3)Choose two of the genres below that you enjoy the most:\na)comedy\nb)action\nc)drama\nd)horror\n")
+    favourites.append(input("answer:"))
     #question 4
-    print("4) What is your favourite animation ever?(if you don't have one write:I don't watch animations)\n")
+    print("\n4)Which one of these plots seems more interesting to you?\na)A mafia patriarch transfers control of his empire to his reluctant son")
+    print("b)A tragic love stroy unfolds abroad the ill-fated Titanic ship")
+    print("c)Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency")
+    print("d)Genetically engineered dinosaurs wreak havoc after scaping in a theme park preview\n")
+    favourites.append(input("answer:"))
     #question 5
-    print("5) What experience are you looking for when watching a movie?\na)a relaxing time\nb)a new concept to think about\nc)to learn about people and life\nd)excitement or fun\n")
+    print("\n5)What experience are you looking for when watching a movie?\na)a relaxing time\nb)a new concept to think about\nc)to learn about people and life")
+    print("d)excitement or fun\n")
+    favourites.append(input("answer:"))
     #question 6
-    print("6) Who is your favorite actor or actresse?(Name one only.)\n")
+    print("\n6)Which one of these actors/actresses have you previously seen in a movie)Tom Hanks\nb)Meryl Streep\nc)Leonardo Dicaprio\nd)none\n")
+    favourites.append(input("answer:"))
     #question 7
-    print("7) Which one of the directors below would you want to meet in real life?\na)Steven Spielberg\nb)Martin Scorsese\nc)Quentin Tarantino\nd)Christopher Nolan\n")
+    print("\n7)Which one of the directors below would you want to meet in real life?\na)Steven Spielberg\nb)Martin Scorsese\nc)Quentin Tarantino\nd)Christopher Nolan\n")
+    favourites.append(input("answer:"))
     #question 8
-    print("8) What is your preferred movie length?\na)a shorter film(~90 minutes)\nb)average length(~1.5 to 2 hours)\nc)a long movie(~2.5 hours or more)\n")
+    print("\n8)What is your preferred movie length?\na)a shorter film(~90 minutes)\nb)average length(~1.5 to 2 hours)\nc)a long movie(~2.5 hours or more)\n")
+    favourites.append(input("answer:"))
     #question 9
-    print("9) Are you in the mood for something light-hearted or serious?\na)light-hearted\nb)serious\n")
+    print("\n9)Are you in the mood for something light-hearted or serious?\na)light-hearted\nb)serious\n")
+    favourites.append(input("answer:"))
     #question 10
-    print("10) I want to watch a movie from ...?\na)this year\nb)the past 2 years\nc)the past 5 years\nd)the past 10 years\n")
+    print("\n10)I want to watch a movie from ...?\na)this year\nb)the past 2 years\nc)the past 5 years\nd)the past 10 years\n")
+    favourites.append(input("answer:"))
 
-profile = {}
+#starting, creating a profile, verifying the info then finding what the user's taste is in movies.
+p = {}
 welcome_page()
-create_profile(profile)
-show_profile()
-verify_information()
+create_profile(p)
+show_profile(p)
+
+#list of user's favourite things = f
+f=[]
+verify_information(p,f)
+#suggesting top 5 movies for this user:
+
 
     
 
