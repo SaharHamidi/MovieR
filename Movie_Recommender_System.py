6f5cce9f5050758f89849733ac129abe503129b4
nargesr564 <nargesrezaiiiii@gmail.com>
2024-05-08 20:35:45 +0330

    reordered questions & deleted extra files

--- Movie_Recommender_System.py
+++ Movie_Recommender_System.py
@@ -8,6 +8,7 @@ movies={"Inception":[2010,9,"Christopher_Nolan",["Leonardo_Dicaprio","Cillian_Mu
 #series={"movie name":[year,IMDB,director,cast,genre,time,rotten tomatoes]}
 series={"Harry_Potter":[2001,8,"Chris_Columbus",["Daniel_Radcliffe","Emma_Watson","Rupert_Grint"],["fantasy","drama"],1172,84]}
 
+#fix this and make a max function out of it
 points=[2,3,4,10,9,8,7,5,7,8]
 max=0
 count=0
@@ -81,44 +82,58 @@ def questionnaire(favourites=[]):
     print("\nLet's find out what your preferences are.")
     print("Please answer the next 10 questions by choosing one of the options below.")
     print("(for example:a)\n")
+
     #question 1
-    print("1)Do you prefer popular movies or indie films?\na)very popular shows\nb)indie movies\nc)doesn't matter to me\nd)both\n")
-    answer=input("answer:")
-        
-    #favourites.append(input("answer:"))
+    print("1)I want to watch a movie from ...?\na)this year\nb)the past 2 years\nc)the past 5 years\nd)the past 10 years\n")
+    favourites.append(input("answer:"))
+
     #question 2
     print("\n2)Which rating do you think is most valid?\na)IMDB\nb)Rotten Tomatos\nc)neither\nd)both\n")
     #favourites.append(input("answer:"))
-    if answer='a':
+    if answer=='a':
         favourites.append(7)
+        
     #question 3
-    print("\n3)Choose two of the genres below that you enjoy the most:\na)comedy\nb)action\nc)drama\nd)horror\n")
+    print("\n3)Which one of the directors below would you want to meet in real life?")
+    print("a)Steven Spielberg\nb)Martin Scorsese\nc)Quentin Tarantino\nd)Christopher Nolan\n")
     favourites.append(input("answer:"))
+    
     #question 4
-    print("\n4)Which one of these plots seems more interesting to you?\na)A mafia patriarch transfers control of his empire to his reluctant son")
+    print("\n4)Which one of these actors/actresses have you previously seen in a movie)Tom Hanks\nb)Meryl Streep\nc)Leonardo Dicaprio\nd)none\n")
+    favourites.append(input("answer:"))
+
+    #question 5
+    print("\n5)Choose two of the genres below that you enjoy the most:\na)comedy\nb)action\nc)drama\nd)horror\n")
+    favourites.append(input("answer:"))
+
+    #question 6
+    print("\n6)What is your preferred movie length?\na)a shorter film(~90 minutes)\nb)average length(~1.5 to 2 hours)\nc)a long movie(~2.5 hours or more)\n")
+    favourites.append(input("answer:"))
+    
+    #--------------------------
+    #what do I do with these??
+    #--------------------------
+    #question 7
+    print("\n7)Do you prefer popular movies or indie films?\na)very popular shows\nb)indie movies\nc)doesn't matter to me\nd)both\n")
+    answer=input("answer:")    
+    #favourites.append(input("answer:"))
+    
+    #question 8
+    print("\n8)Which one of these plots seems more interesting to you?\na)A mafia patriarch transfers control of his empire to his reluctant son")
     print("b)A tragic love stroy unfolds abroad the ill-fated Titanic ship")
     print("c)Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency")
     print("d)Genetically engineered dinosaurs wreak havoc after scaping in a theme park preview\n")
     favourites.append(input("answer:"))
-    #question 5
-    print("\n5)What experience are you looking for when watching a movie?\na)a relaxing time\nb)a new concept to think about\nc)to learn about people and life")
+
+    #question 9
+    print("\n9)What experience are you looking for when watching a movie?\na)a relaxing time\nb)a new concept to think about\nc)to learn about people and life")
     print("d)excitement or fun\n")
     favourites.append(input("answer:"))
-    #question 6
-    print("\n6)Which one of these actors/actresses have you previously seen in a movie)Tom Hanks\nb)Meryl Streep\nc)Leonardo Dicaprio\nd)none\n")
-    favourites.append(input("answer:"))
-    #question 7
-    print("\n7)Which one of the directors below would you want to meet in real life?\na)Steven Spielberg\nb)Martin Scorsese\nc)Quentin Tarantino\nd)Christopher Nolan\n")
-    favourites.append(input("answer:"))
-    #question 8
-    print("\n8)What is your preferred movie length?\na)a shorter film(~90 minutes)\nb)average length(~1.5 to 2 hours)\nc)a long movie(~2.5 hours or more)\n")
-    favourites.append(input("answer:"))
-    #question 9
-    print("\n9)Are you in the mood for something light-hearted or serious?\na)light-hearted\nb)serious\n")
-    favourites.append(input("answer:"))
+
     #question 10
-    print("\n10)I want to watch a movie from ...?\na)this year\nb)the past 2 years\nc)the past 5 years\nd)the past 10 years\n")
+    print("\n10)Are you in the mood for something light-hearted or serious?\na)light-hearted\nb)serious\n")
     favourites.append(input("answer:"))
+    
 
 #starting, creating a profile, verifying the info then finding what the user's taste is in movies.
 p = {}
@@ -131,5 +146,5 @@ f=[]
 verify_information(p,f)
 #suggesting top 5 movies for this user:
 
-def compare(fav=[],movies[]):
-        
+def compare(fav=[],movies=[]):
+        print("something")
